84b0a4bed359d94e3d3c7916f092e698eabe1a06
91878 <91878@MonuTop>
2025-06-17 11:53:11 +0530

    This is java project

---
+++ ALL/src/all/Main.java
@@ -0,0 +1,13 @@
+package all;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a = 10;
+		int b = 5;
+		System.out.println(a+b);
+
+	}
+
+}
