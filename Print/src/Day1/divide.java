b1d3318abbfdf2c4788a58ffead52983654cc484
91878 <91878@MonuTop>
2025-06-19 14:29:28 +0530

    this is my core java program

---
+++ Print/src/Day1/divide.java
@@ -0,0 +1,12 @@
+package Day1;
+
+public class divide {
+
+	public static void main(String[] args) {
+         int a = 10;
+         int b = 5;
+         int divide = (a/b);
+         System.out.println(divide);
+	}
+
+}
